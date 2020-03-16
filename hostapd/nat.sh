58bb89e27224f1750f4c3c81bf4cf9a016048766
Benedict Lau <b@deprecated.systems>
2020-03-16 11:39:34 +0100

    Fix dnsmasq and remove radvd

--- hostapd/nat.sh
+++ hostapd/nat.sh
@@ -2,8 +2,6 @@
 
 # MSS clamp to circumvent issues with Path MTU Discovery
 iptables -A FORWARD -p tcp --tcp-flags SYN,RST SYN -j TCPMSS --clamp-mss-to-pmtu
-ip6tables -A FORWARD -p tcp --tcp-flags SYN,RST SYN -j TCPMSS --clamp-mss-to-pmtu
 
 # Forward all traffic from the internal network to the eth0 device and mask with the eth0 external IP address
 iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
-ip6tables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
